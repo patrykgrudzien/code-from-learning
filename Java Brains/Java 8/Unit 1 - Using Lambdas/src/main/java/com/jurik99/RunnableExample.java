1e7c39e0f2a2c57a0d30438578e0932491cbdb12
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2017-04-03 22:22:41 +0200

    Working with unit 1 content

---
+++ Java Brains/Java 8/Unit 1 - Using Lambdas/src/main/java/com/jurik99/RunnableExample.java
@@ -0,0 +1,22 @@
+package com.jurik99;
+
+public class RunnableExample
+{
+	public static void main(String[] args)
+	{
+		final Thread myThread = new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				System.out.println("Printed inside Runnable");
+			}
+		});
+
+		myThread.start();
+
+		// --- LAMBDA ---
+		final Thread myLambdaThread = new Thread(() -> System.out.println("Printed in lambda expression"));
+		myLambdaThread.start();
+	}
+}
---
+++ Java Brains/Java 8/Unit 1 - Using Lambdas/src/main/java/com/jurik99/Unit1Exercise.java
@@ -0,0 +1,22 @@
+package com.jurik99;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise
+{
+	public static void main(String[] args)
+	{
+		final List<Person> people = Arrays.asList(
+				new Person("Imie 1", "Nowak", 10),
+				new Person("Imie 2", "Grudzien", 20),
+				new Person("Imie 3", "Stawiarz", 30),
+				new Person("Imie 4", "Adamski", 40));
+
+		// --- Step 1. Sort list by last name ---
+
+		// --- Step 2. Create method that prints all elements in the list ---
+
+		// --- Step 3. Create a method that prints all people that have name beginning with C ---
+	}
+}
