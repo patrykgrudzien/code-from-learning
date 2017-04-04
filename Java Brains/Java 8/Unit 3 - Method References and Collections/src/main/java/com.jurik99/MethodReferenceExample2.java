03c83fbd8b8617c829e3926f3663ea80d097260f
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2017-04-05 00:35:27 +0200

    Started working on unit 3

---
+++ Java Brains/Java 8/Unit 3 - Method References and Collections/src/main/java/com.jurik99/MethodReferenceExample2.java
@@ -0,0 +1,39 @@
+package com.jurik99;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class MethodReferenceExample2
+{
+	@SuppressWarnings("Duplicates")
+	public static void main(String[] args)
+	{
+		final List<Person> people = Arrays.asList(new Person("Patryk", "Nowak", 10),
+		                                          new Person("Damian", "Grudzien", 20),
+		                                          new Person("Celina", "Stawiarz", 30),
+		                                          new Person("Viola", "Adamski", 40),
+		                                          new Person("Amadeusz", "Chmmielewski", 50));
+
+		// --- Print all elements in the list ---
+		System.out.println("Printing all people:");
+		performConditionally(people, person -> true, System.out::println);
+		/*
+		 * System.out - it's an INSTANCE
+		 * println() - it's a method which is called on that instance (System.out)
+		 */
+	}
+
+	private static void performConditionally(final List<Person> people, final Predicate<Person> predicate,
+	                                         final Consumer<Person> consumer)
+	{
+		for (final Person person : people)
+		{
+			if (predicate.test(person))
+			{
+				consumer.accept(person);
+			}
+		}
+	}
+}
