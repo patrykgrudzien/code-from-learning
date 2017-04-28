67430c1359aa51efcac085baa14cdec223528d15
patrykgrudzien <patryk.grudzien.91@gmail.com>
2017-04-28 16:52:13 +0200

    Changed module name. Created structure in Unversioned Code module.

---
+++ Journal Dev/Design Patterns/Structural/src/main/java/com.jurik99/Adapter/Intro.java
@@ -0,0 +1,20 @@
+package com.jurik99.Adapter;
+
+public class Intro
+{
+	/*
+	 *  Adapter design pattern is one of the STRUCTURAL DESIGN PATTERN and its used so that two unrelated interfaces
+	 *  can work together. The object that joins these unrelated interface is called an ADAPTER.
+	 *
+	 *  One of the great real life example of Adapter pattern is mobile charger. Mobile baterry needs 3 volts to
+	 *  charge but the normal socket produces either 120V (US) or 240V (India). So the mobile charger works as an
+	 *  adapter between mobile charging socket and the wall socket.
+	 *
+	 *  Step-by-step:
+	 *  1) 2 classes - Volt, Socket
+	 *  2) SocketAdapter interface
+	 *  3) SocketClassAdapterImpl
+	 *  4) SocketObjectAdapterImpl
+	 *  5) AdapterPatternTest
+	 */
+}
