7fd9e869a95e62a10599cafe02c356420897bd61
patrykgrudzien <patryk.grudzien.91@gmail.com>
2017-10-11 17:03:47 +0200

    Added Swagger API to book search ajax application.

--- Tutorials/Youtube/Virtual Pair Programmers/SpringMVC - Ajax and JSON/src/main/java/com/jurik99/controller/BookController.java
+++ Tutorials/Youtube/Virtual Pair Programmers/SpringMVC - Ajax and JSON/src/main/java/com/jurik99/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.jurik99.controller;
 
+import io.swagger.annotations.Api;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,6 +15,7 @@ import com.jurik99.model.Book;
 import com.jurik99.service.BookService;
 
 @Controller
+@Api(value = "bookSearcher", description = "Book Controller responsible for AJAX searching")
 public class BookController {
 
 	private BookService bookService;
@@ -21,7 +25,7 @@ public class BookController {
 		this.bookService = bookService;
 	}
 
-	@RequestMapping("/performLooseSearch")
+	@RequestMapping(value = "/performLooseSearch", method = RequestMethod.GET)
 	public @ResponseBody List<Book> performLooseSearch(@RequestParam("CHARS") final String chars) {
 		return bookService.searchBooksByLooseMath(chars);
 	}
