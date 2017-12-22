7fdbbb44afda310bb2d3c122f39e6588e3342474
patrykgrudzien <patryk.grudzien.91@gmail.com>
2017-12-22 09:59:25 +0100

    Java 8 examples #2

---
+++ Found in the Internet/Java 8/ConcretePage/BiConsumer-BiFunction-BiPredicate/src/main/java/com/jurik99/BiConsumerExample1.java
@@ -0,0 +1,15 @@
+package com.jurik99;
+
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample1 {
+
+	public static void main(final String[] args) {
+
+		BiConsumer<Integer, Integer> addition = (integer1, integer2) -> System.out.println(integer1 + integer2);
+		BiConsumer<Integer, Integer> subtraction = (integer1, integer2) -> System.out.println(integer1 - integer2);
+
+		// --- using "andThen()" ---
+		addition.andThen(subtraction).accept(10, 6);    // output: 16, 4
+	}
+}
--- Found in the Internet/Java 8/ConcretePage/BiConsumer-BiFunction-BiPredicate/src/main/java/com/jurik99/BiConsumerExample2.java
+++ Found in the Internet/Java 8/ConcretePage/BiConsumer-BiFunction-BiPredicate/src/main/java/com/jurik99/BiConsumerExample2.java
@@ -1,15 +1,61 @@
 package com.jurik99;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class BiConsumerExample2 {
 
+	// --- TESTING IN "Person" CLASS BELOW ---
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+class Person {
+
+	private String name;
+	private String lastName;
+	private String email;
+	private String address;
+
+	private Person(final String name, final String lastName) {
+		this.name = name;
+		this.lastName = lastName;
+	}
+
+	// --- Use the Person's method references and assign them to functions ---
+
+	// --- GETTERS ---
+	private static Function<Person, String> getNameFunction = Person::getName;
+	private static Function<Person, String> getLastNameFunction = Person::getLastName;
+	private static Function<Person, String> getEmailFunction = Person::getEmail;
+	private static Function<Person, String> getAddressFunction = Person::getAddress;
+
+	// --- SETTERS ---
+	private static BiConsumer<Person, String> setNameFunction = Person::setName;
+	private static BiConsumer<Person, String> setLastNameFunction = Person::setLastName;
+	private static BiConsumer<Person, String> setEmailFunction = Person::setEmail;
+	private static BiConsumer<Person, String> setAddressFunction = Person::setAddress;
+
+	// --- DEFAULT CONSTRUCTOR ---
+	private static Supplier<Person> defaultConstructor = Person::new;
+
+	// --- TWO ARGS CONSTRUCTOR ---
+	private static BiFunction<String, String, Person> twoArgsConstructor = Person::new;
+
+
+	// --- MAIN ---
 	public static void main(final String[] args) {
 
-		BiConsumer<Integer, Integer> addition = (integer1, integer2) -> System.out.println(integer1 + integer2);
-		BiConsumer<Integer, Integer> subtraction = (integer1, integer2) -> System.out.println(integer1 - integer2);
+		final Person person = twoArgsConstructor.apply("Patryk", "Grudzien");
+		System.out.println(getLastNameFunction.apply(person));
 
-		// --- using "andThen()" ---
-		addition.andThen(subtraction).accept(10, 6);    // output: 16, 4
+		setAddressFunction.accept(person, "Address");
 	}
 }
