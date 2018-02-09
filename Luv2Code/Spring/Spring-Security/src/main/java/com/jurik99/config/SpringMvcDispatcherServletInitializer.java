ce8261cc955339ca3f856f24c78fa50ae14f792e
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-02-09 19:39:35 +0100

    spring-security-demo-01-base-app

---
+++ Luv2Code/Spring/Spring-Security/src/main/java/com/jurik99/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,30 @@
+package com.jurik99.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/*
+ * WEB APP INITIALIZER
+ *
+ * 1) Spring MVC provides support for web app initialization
+ * 2) Makes sure your code is automatically detected
+ * 3) Your code is used to initialize the servlet container
+ */
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[0];
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// --- our config class created for SERVLET (to handle view .jsp files) ---
+		return new Class[] {SpringServletAndWebXmlConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// --- default mapping for DISPATCHER SERVLET ---
+		return new String[] {"/"};
+	}
+}
--- Luv2Code/Spring/Spring-Security/src/main/java/com/jurik99/config/SpringServletAndWebXmlConfiguration.java
+++ Luv2Code/Spring/Spring-Security/src/main/java/com/jurik99/config/SpringServletAndWebXmlConfiguration.java
@@ -1,19 +1,26 @@
 package com.jurik99.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+/*
+ *  @EnableWebMvc:
+ *  1) Provides similar support to <mvc:annotation-driven /> in XML
+ *  2) Adds conversion, formatting and validation support
+ *  3) Processing of @Controller classes and @RequestMapping etc... methods
+ *
+ *  "WebMvcConfigurerAdapter" abstract class has been DEPRECATED cause now in "WebMvcConfigurer" interface we have a lot of
+ *  default methods which can be simply OVERRIDDEN :)
+ */
 @Configuration
 @EnableWebMvc
-/*
- *  "WebMvcConfigurerAdapter" abstract class has been DEPRECATED cause now in "WebMvcConfigurer" interface we have a lot of
- *  default methods which can be simply OVERRIDEN :)
- */
-public class ViewResolverConfig implements WebMvcConfigurer {
+@ComponentScan(basePackages = "com.jurik99")
+public class SpringServletAndWebXmlConfiguration implements WebMvcConfigurer {
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
